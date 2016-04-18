0830a39ad4bf384b3639988e9c31412ca0a39356
Michal Rutka <michal.rutka@gmail.com>
2016-04-18 16:52:56 +0200

    EnOcean press release in EepF60201 + new profile EepA52001

--- controller/src/main/java/org/openremote/controller/protocol/enocean/profile/EepType.java
+++ controller/src/main/java/org/openremote/controller/protocol/enocean/profile/EepType.java
@@ -23,6 +23,7 @@ package org.openremote.controller.protocol.enocean.profile;
 import org.openremote.controller.protocol.enocean.ConfigurationException;
 import org.openremote.controller.protocol.enocean.DeviceID;
 import org.openremote.controller.protocol.enocean.packet.radio.EspRadioTelegram;
+import org.openremote.controller.protocol.enocean.packet.radio.EspRadioTelegram.RORG;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -393,6 +394,11 @@ public abstract class EepType
    * EnOcean equipment profile (EEP) 'A5-12-03' type.
    */
   public final static EepType EEP_TYPE_A51203 = new EepTypeA51203();
+
+  /**
+   * EnOcean equipment profile (EEP) 'A5-20-01' type.
+   */
+  public final static EepType EEP_TYPE_A52001 = new EepTypeA52001();
 
   /**
    * End of FUNC value range: {@value}
@@ -2176,6 +2182,25 @@ public abstract class EepType
     @Override public Eep createEep(DeviceID deviceID, String command) throws ConfigurationException
     {
       return new EepA51203(deviceID, command);
+    }
+  }
+  
+  private static class EepTypeA52001 extends EepType
+  {
+    /**
+     * Constructs a new instance for the EEP number 'A5-20-01' and puts it into the
+     * lookup table of all supported profiles.
+     */
+    public EepTypeA52001() {
+      super(EspRadioTelegram.RORG.BS4, 0x20, 0x01);
+    }
+ 
+    /**
+     * {@inheritDoc}
+     */
+    @Override public Eep createEep(DeviceID deviceID, String command) throws ConfigurationException
+    {
+      return new EepA52001(deviceID, command);
     }
   }
 
